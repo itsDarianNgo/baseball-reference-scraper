9513543816acbc22222a3d7fa494eab25a73078d
itsdarianngo <darianngo@gmail.com>
2023-06-14 01:45:57 -0500

    ScraperV2 scraping data correctly

---
+++ scraperV2.py
@@ -0,0 +1,117 @@
+import os
+import csv
+import time
+import requests
+import logging
+from retrying import retry
+from bs4 import BeautifulSoup
+
+# Set up logging
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+# Retry parameters
+retry_params = {
+    "stop_max_attempt_number": 3,
+    "wait_exponential_multiplier": 1000,
+    "wait_exponential_max": 10000,
+}
+
+
+@retry(**retry_params)
+def get_page_content(url, proxies=None):
+    response = requests.get(url, proxies=proxies)
+
+    # Handle rate limiting
+    if response.status_code == 429:
+        logging.error("Rate limit reached. Sleeping for a minute...")
+        time.sleep(60)
+        return get_page_content(url, proxies=proxies)
+
+    return BeautifulSoup(response.text, "html.parser")
+
+
+def get_boxscore_links(year, proxies=None):
+    url = f"https://www.baseball-reference.com/leagues/majors/{year}-schedule.shtml"
+    soup = get_page_content(url, proxies=proxies)
+    boxscore_links = [link["href"] for link in soup.find_all("a", string="Boxscore")]
+    return boxscore_links
+
+
+def parse_boxscore_page(url):
+    response = requests.get(
+        url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"}
+    )
+    if response.status_code != 200:
+        raise Exception(f"Failed to load page {url}")
+
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    # Extract home and away teams from the page title
+    title = soup.find("title").text
+    teams = title.split(" vs ")
+    away_team = teams[0]
+    home_team = teams[1].split(" Box Score: ")[0]
+
+    # Extract scores from the team info section
+    score_info = soup.find("div", {"class": "scorebox"}).find_all("div", {"class": "scores"})
+    away_score = score_info[0].find("div", {"class": "score"}).text
+    home_score = score_info[1].find("div", {"class": "score"}).text
+
+    # Determine the winner
+    winner = home_team if int(home_score) > int(away_score) else away_team
+
+    # Extract additional game details
+    game_details = soup.find("div", {"class": "scorebox_meta"}).find_all("div")
+    venue = game_details[3].text.split(": ")[1]
+    game_duration = game_details[4].text.split(": ")[1]
+
+    # Extract date from the title
+    date = title.split(" Box Score: ")[1]
+
+    # Extract game ID from the URL
+    game_id = url.split("/")[-1].split(".")[0]
+
+    # Create a dictionary to store the extracted data
+    game_data = {
+        "game_id": game_id,
+        "date": date,
+        "home_team": home_team,
+        "away_team": away_team,
+        "home_score": home_score,
+        "away_score": away_score,
+        "winner": winner,
+        "venue": venue,
+        "game_duration": game_duration,
+    }
+
+    return game_data
+
+
+def get_game_data(year, proxies=None):
+    boxscore_links = get_boxscore_links(year, proxies=proxies)
+    data_dir = f"data/{year}"
+    os.makedirs(data_dir, exist_ok=True)
+
+    for i, link in enumerate(boxscore_links, start=1):
+        boxscore_url = f"https://www.baseball-reference.com{link}"
+        game_data = parse_boxscore_page(boxscore_url)
+
+        # Save game data to a CSV file
+        with open(f"{data_dir}/game_{i}.csv", "w", newline="") as f:
+            writer = csv.DictWriter(f, fieldnames=game_data.keys())
+            writer.writeheader()
+            writer.writerow(game_data)
+
+        logging.info(f"Scraped and saved data for game {i} of year {year}.")
+
+        # Respect rate limiting
+        time.sleep(3)
+
+
+# Load proxies from a text file
+# with open('proxies.txt') as f:
+#     proxies = [line.strip() for line in f]
+# proxy_pool = cycle(proxies)
+
+# Example usage:
+get_game_data(2022)
